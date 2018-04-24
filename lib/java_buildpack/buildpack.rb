6a97ca2b6a1b63b0db4e11255484622035ac27d3
Anusheel Tandon <atandon@altimetrik.com>
2018-04-24 16:33:49 +0530

    #

--- lib/java_buildpack/buildpack.rb
+++ lib/java_buildpack/buildpack.rb
@@ -257,10 +257,14 @@ module JavaBuildpack
       # @yield [Buildpack] the buildpack to work with
       # @return [Object] the return value from the given block
       def with_buildpack(app_dir, message)
-		logger = Logging::LoggerFactory.instance.get_logger Buildpack
-		@logger.debug { "inside with_buildpack" }
+		
+		
         app_dir = Pathname.new(File.expand_path(app_dir))
         Logging::LoggerFactory.instance.setup app_dir
+		if Logging::LoggerFactory.instance.initialized
+			logger = Logging::LoggerFactory.instance.get_logger Buildpack
+			@logger.debug { "inside with_buildpack" }
+		end
         application = Component::Application.new(app_dir)
 
         yield new(app_dir, application) if block_given?
